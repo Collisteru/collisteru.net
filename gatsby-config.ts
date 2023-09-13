5d7d1240b6af195cd58fa69f80839d3e46807f41
Sean Carter <seca6364@colorado.edu>
2023-09-13 09:46:13 -0600

    Build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -34,7 +34,14 @@ const config: GatsbyConfig = {
     options: {
       "icon": "src/images/icon.png"
     },
-  }, "gatsby-transformer-remark", {
+  },
+  {
+    resolve: "gatsby-plugin-page-creator",
+    options: {
+      path: `./src/pages/`, // Set the path to your custom pages directory
+    } 
+  },
+   "gatsby-transformer-remark", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
