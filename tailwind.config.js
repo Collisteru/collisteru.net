32e9bf4ecd06c3b47242c4309af296bde5a01d1c
Sean Carter <seca6364@colorado.edu>
2024-01-10 14:11:10 -0700

    Added icon, banner. Simple page layout.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: '#800020',
+        gold: '#FFD700',
+        green: '#013220',
+        light_blue: '#000080',
+        dark_blue: '#261C80',
+        purple: '#714194',
+        yellow: '#CCA278',
+        brown: '#541D01',
+        white: '#EDECE1',
+        black: '0F0F0F'
+      }
+    },
   },
   plugins: [],
 }
