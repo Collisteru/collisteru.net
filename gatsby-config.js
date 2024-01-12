53fdc85d55210a22a7d21c638a78dcdce709b85a
Sean Carter <seca6364@colorado.edu>
2024-01-11 23:37:38 -0700

    Removed Google fonts to fix error

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,19 +30,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Charis SIL`,
-            subsets: [`regular`, 'bold', 'italic', 'bold italic'],  // Adjust subsets as needed
-          },
-          // Add more fonts as needed
-        ],
-        display: 'swap',
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -137,7 +124,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     
