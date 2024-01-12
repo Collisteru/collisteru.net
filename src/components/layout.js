006bda8dd8dee033ed920c1fbec5afed94c85dba
Sean Carter <seca6364@colorado.edu>
2024-01-11 19:01:55 -0700

    A great deal of formatting progress!

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ const Layout = ({ location, title, children }) => {
       <h1 className="main-heading">
         <Link to="/">{title}</Link>
       </h1>
+
     )
   } else {
     header = (
@@ -23,8 +24,8 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
+      <main id="content">{children}</main>
+      <footer className = "global-footer">
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
