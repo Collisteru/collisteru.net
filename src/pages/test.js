1c3103cbb378d5fcfc40e2cf1b26672a76007de5
Sean Carter <seca6364@colorado.edu>
2024-03-29 12:02:15 -0600

    Testing HTML elements

---
+++ src/pages/test.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export const query = graphql`
+  query {
+    markdownRemark(fileAbsolutePath: { regex: "/content/other/test/" }) {
+      html
+    }
+  }
+`
+
+function get_content(data) {
+  const { markdownRemark } = data
+  const { html } = markdownRemark
+  const html_to_render = { __html: html }
+  return html_to_render
+}
+
+const Test = ({ data }) => {
+  const html_to_render = get_content(data)
+
+  return (
+    <Layout>
+      <div dangerouslySetInnerHTML={html_to_render} />
+    </Layout>
+  )
+}
+
+export default Test
